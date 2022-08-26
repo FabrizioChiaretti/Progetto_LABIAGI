14c10b479d9ee1636301e0e14d8cbd6e35100231
FabrizioChiaretti <chiaretti.1918799@studenti.uniroma1.it>
2022-08-26 19:54:36 +0200

    to do roi

--- main.py
+++ main.py
@@ -237,34 +237,58 @@ def genetic_algorithm(mat):
 
 def main ():
     
-
+    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("image", 700, 700)
+    
     for img in glob.glob("Sudoku/Mat1/matrix1.jpg"): #"Sudoku/Mat1/matrix.jpg" 
-        image = cv2.imread(img, cv2.COLOR_BGR2HSV)
+        image = cv2.imread(img, cv2.IMREAD_COLOR)
     
     cv2.imshow("image", image)
     cv2.waitKey(0)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   
-    cv2.imshow("gray image", image)
-    cv2.waitKey(0)
-    #ret,image = cv2.threshold(image,127,255,cv2.THRESH_TOZERO)
-    #cv2.imshow("image thresh", image)
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow("image", image)
     #cv2.waitKey(0)
-    roi = cv2.selectROI(image)
-    print(roi)
-    roi_cropped = image[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
-    cv2.imshow("roi cropped", roi_cropped)
+    
+    ret,image = cv2.threshold(image, 210, 255, cv2.THRESH_BINARY)
+    #cv2.imshow("image", image)
+    #cv2.waitKey(0)
+    
+    cnts, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image, cnts, -1, (0,255,0), 3)
+    cv2.imshow("image", image)
     cv2.waitKey(0)
-    sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    sharpen = cv2.filter2D(roi_cropped, -1, sharpen_kernel)
-    cv2.imshow('sharpen', sharpen)
-    cv2.waitKey()
+    
+    ROI_number = 0
+    for i in range(len(cnts)):
+        if (i % 2 == 0):
+            cnt = cnts[i]
+            x,y,w,h = cv2.boundingRect(cnt)
+            ROI = image[y:y+h, x:x+w]
+            cv2.imshow("ROI", ROI)
+            cv2.waitKey(0)
+            ROI_number += 1
+
+    print(ROI_number) #dovrebbe essere uguale a 9x9=81, invece è uguale a 79
+    
+    #laplacian = cv2.Laplacian(image,cv2.CV_64F)
+    #cv2.imshow("laplacian image", laplacian)
+    #cv2.waitKey(0)
+    #roi = cv2.selectROI(image)
+    #roi_cropped = image[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+    #cv2.imshow("roi cropped", roi_cropped)
+    #cv2.waitKey(0)
+    #sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    #sharpen = cv2.filter2D(roi_cropped, -1, sharpen_kernel)
+    #cv2.imshow('sharpen', sharpen)
+    #cv2.waitKey()
     #image = cv2.GaussianBlur(img_thresh, (3,3), 0)
     #kernel = np.ones((5, 5), np.uint8)
     #image = cv2.erode(blur_img, kernel) 
     #cv2.imshow("blur_img", image)
     #cv2.waitKey(0)
-    text = pytesseract.image_to_string(sharpen)
-    print(text)
+    #text = pytesseract.image_to_string(sharpen)
+    #print(text)
     cv2.destroyAllWindows()
     
     
